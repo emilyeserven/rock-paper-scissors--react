3ecd004cbe010f34d4980bf4d0f373e576943acb
Emily Serven <emily.e.serven@gmail.com>
2021-08-09 15:46:59 -0400

    General main structure imported for converting to components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div class="button-container">
+    <div className="game-container">
+      <h1>Rock, Paper, Scissors!</h1>
+      <div className="button-container">
         <div id="rock" className="option">
             <img src="/assets/rock.png" /><br />
             <span className="label">Rock</span>
@@ -17,7 +18,32 @@ function App() {
             <img src="/assets/scissor.png" /><br />
             <span className="label">Scissor</span>
         </div>
-    </div>
+      </div>
+      <div className="winnings">
+          <div className="result winBox">
+              <h2>Results</h2>
+              <div className="pickContainer">
+                  <span className="pickText">You: </span><span id="userPick"></span>
+              </div>
+              <div className="pickContainer">
+                  <span className="pickText">Computer: </span><span id="compPick"></span>
+              </div>
+              <div className="clearfix"></div>
+              <div className="resultContainer">
+                  <span id="overallResult">Click for a result!</span>
+              </div>
+          </div>
+          <div className="scores winBox">
+              <h2>Score</h2>
+              <div className="userScore score-box">
+                  User Score is now: <span id="userScore">0</span>
+              </div>
+              <div className="compScore score-box">
+                  Computer Score is now: <span id="compScore">0</span>
+              </div>
+          </div>
+      </div>
+      <sub className="center"><a href="https://icons8.com/web-app/tag/rock-paper-scissor">Icons from icons8</a></sub>
     </div>
   );
 }
